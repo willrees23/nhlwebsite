a1ca68478e4821550623bffb34adb3bc5799498f
xWand <will.rees9132@gmail.com>
2024-03-05 18:57:18 +0000

    Added nhl assets page for images

--- next.config.js
+++ next.config.js
@@ -17,6 +17,13 @@ const config = {
     locales: ["en"],
     defaultLocale: "en",
   },
+  images: {
+    remotePatterns: [
+      {
+        hostname: "assets.nhle.com",
+      },
+    ],
+  },
 };
 
 export default config;
