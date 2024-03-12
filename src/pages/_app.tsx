2539194ecfc6210c13bb2fdf3d7b2ca7e83d2840
xWand <will.rees9132@gmail.com>
2024-03-12 07:54:39 +0000

    gitignore

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -28,23 +28,6 @@ const MyApp: AppType<{ session: Session | null }> = ({
         <meta name="msapplication-tap-highlight" content="no" />
         <meta name="theme-color" content="#000000" />
 
-        <link rel="apple-touch-icon" href="/icons/touch-icon-iphone.png" />
-        <link
-          rel="apple-touch-icon"
-          sizes="152x152"
-          href="/icons/touch-icon-ipad.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/icons/touch-icon-iphone-retina.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="167x167"
-          href="/icons/touch-icon-ipad-retina.png"
-        />
-
         <link
           rel="icon"
           type="image/png"
